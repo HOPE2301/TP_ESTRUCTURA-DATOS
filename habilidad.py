1e9d2d136d736610757da8889cf9a739d835faa8
Yanina cañette <yanina_belen_c@hotmail.com>
2025-10-15 11:46:27 -0300

    Update and rename class_habilidad to habilidad.py

---
+++ habilidad.py
@@ -0,0 +1,7 @@
+Class habilidad():
+      def __init__(self, nombre,nivel_habilidad):
+            self_nombre = nombre
+            self_ nivel_habilidad = nivel_ habilidad 
+
+      def datos(self):
+            print (f"la habilidad {self_nombre} tiene  un nivel de {self_nivel_habilidad} ")
