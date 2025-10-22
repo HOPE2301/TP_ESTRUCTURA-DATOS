87d715860209a5856d03edbbba1c687e543e15c4
Yanina cañette <yanina_belen_c@hotmail.com>
2025-10-22 14:05:55 -0300

    clase Habilidad y sus respectivas funciones
    Refactor Personaje class to separate Habilidad class and update methods.

--- personaje.py
+++ personaje.py
@@ -1,49 +1,25 @@
-class Personaje:
-      def __init__(self,nombre,especie,planeta,nivel_poder=0):
-            self.nombre = nombre
-            self.especie = especie
-            self.planeta = planeta
-            self.habilidad = []
-            self.inventario = []
-            self.nivel_poder = nivel_poder
+class Habilidad:
+    def __init__(self,nombre,nivel_habilidad):
+        self.nombre = nombre
+        self.nivel_habilidad = nivel_habilidad
 
-      def get_nombre (self):
-            return self.nombre
 
-      def set_nombre(self,nombre):
-            self.nombre = nombre 
+    def get_nombre(self):
+        return self.nombre
+    
+    def set_nombre(self,nombre):
+        self.nombre = nombre   
 
-      def get_especie (self):
-            return self.especie
 
-      def set_especie(self,especie):
-            self.especie = especie         
+    def get_nivel_habilidad(self):
+        return self.nivel_habilidad
+    
+    def set_nivel_habilidad(self,nivel_habilidad):
+        self.nivel_habilidad = nivel_habilidad
 
-      def get_planeta(self):
-            return self.planeta
-
-      def set_planeta(self,planeta):
-            self.planeta = planeta
-
-      #Funcion que muestra la informacion de del personaje.
-      def info(self):
-            print  (f" el personaje: {self.nombre} es de la especie {self.especie} y pertenece al planeta {self.planeta}")
-            if self.habilidad:
-                  print("Habilidades:")
-                  for hab in self.habilidad:
-                        hab.datos()
-            if self.inventario:
-                  print("Inventario:")
-                  for obj in self.inventario:
-                        obj.datos()
-#Explicaion de esta funcion, esta funcion recibe un dato, y verifica si es una habilidad o un objeto, y lo agrega a la lista correspondiente, hasattr() es una funcion que ve si el objeto tiene un metodo que pasa por atributo, y callable() ve si es un metodo que se puede llamar.
-      def agregar(self, dato):
-            if hasattr(dato, "soy_habilidad") and callable(getattr(dato, "soy_habilidad")):
-                  self.habilidad.append(dato)
-            elif hasattr(dato, "soy_objeto") and callable(getattr(dato, "soy_objeto")):
-                  self.inventario.append(dato)
-            else:
-                  raise TypeError("dato debe exponer get_poder (habilidad) o get_utilidad (objeto)")
+    def info(self):
+        print (f" {self.nombre} tiene un nivel de habilidad de: {self.nivel_habilidad}")
+        
 
 
 
