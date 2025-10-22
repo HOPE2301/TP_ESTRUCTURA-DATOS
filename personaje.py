ced58992c197d7d5a37be087f5c6dced7ecb21f4
Yanina cañette <yanina_belen_c@hotmail.com>
2025-10-22 14:11:38 -0300

    Update personaje.py

--- personaje.py
+++ personaje.py
@@ -1,24 +1,63 @@
-class Habilidad:
-    def __init__(self,nombre,nivel_habilidad):
+class Personaje:
+    def __init__(self,nombre,especie,planeta):
         self.nombre = nombre
-        self.nivel_habilidad = nivel_habilidad
+        self.especie = especie
+        self.planeta = planeta
+        self.habilidad = []
+        self.inventario = []
+        self.total_poder = 0
 
-
-    def get_nombre(self):
+    def get_nombre (self):
         return self.nombre
     
     def set_nombre(self,nombre):
-        self.nombre = nombre   
+        self.nombre = nombre
 
-
-    def get_nivel_habilidad(self):
-        return self.nivel_habilidad
+    def get_especie(self):
+        return self.especie
     
-    def set_nivel_habilidad(self,nivel_habilidad):
-        self.nivel_habilidad = nivel_habilidad
+    def set_especie(self,especie):
+        self.especie = especie
 
-    def info(self):
-        print (f" {self.nombre} tiene un nivel de habilidad de: {self.nivel_habilidad}")
+    def get_planeta(self):
+        return self.planeta
+    
+    def set_planeta(self,planeta):
+        self.planeta = planeta
+
+    def agregar_habilidad(self,habilidad):
+        if habilidad in self.habilidad:
+            return f"esta habilidad ya se encuentra!"
+        else:
+            self.habilidad.append(habilidad)
+            return f"habilidad agregada"
+        
+    def quitar_habilidad(self,habilidad):
+        if habilidad in self.habilidad:
+            self.habilidad.remove(habilidad)
+        else:
+            return f"habilidad no encontrada!!"
+        
+    def agregar_objeto(self,objeto):
+        if objeto in self.inventario:
+            return f"este objeto ya se encuentra!"
+        else:
+            self.inventario.append(objeto)
+            return f"objeto agregado"
+        
+    def quitar_objeto(self,objeto):
+        if objeto in self.inventario:
+            return f"objeto no encontrado!!"
+        else:
+            return self.inventario.remove(objeto)
+        
+def datos(self):
+    print(f"NOMBRE: {self.nombre}")
+    print(f"ESPECIE: {self.especie}")
+    print(f"PLANETA: {self.planeta}")
+    print (f"HABILIDADES:")
+    for h in self.habilidad:
+        print(f"--> {h.nombre} : {h.nivel_habilidad}")
         
 
 
