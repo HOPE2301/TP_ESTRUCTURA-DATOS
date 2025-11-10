baed4fa6987a423e4c8e330eab293dd3acd426bf
Yanina cañette <yanina_belen_c@hotmail.com>
2025-11-10 16:32:07 -0300

    Update habilidad.py

--- src/habilidad.py
+++ src/habilidad.py
@@ -19,7 +19,7 @@ class Habilidad:
         print (f" {self.nombre} tiene un nivel de habilidad {self.nivel_habilidad}: {self.nivelNum}")
         
 class Transformacion(Habilidad):
-    def __init__(self, nombre,nivel_habilidad):
+    def __init__(self, nombre,nivel_habilidad,multiplicador):
         #llamamos al nombre y nivel_habilidad del constructor de Habilidad:
         super().__init__(nombre,nivel_habilidad)
         self.multiplicador = multiplicador
