a1aa01ee5dcceb69b01670497fb5c97bc8715230
yanina cañette <yanina_belen_c@hotmail.com>
2025-11-28 23:10:22 -0300

    organizacion de archivos en carpetas(limpieza y movimiento final)

--- src/arboles/nodo_ge.py
+++
@@ -1,7 +0,0 @@
-class geNodo:
-    def __init__(self,datos):
-        self.datos = datos
-        self.hijos = [] 
-        
-    def agregar_hijo(self, nodo_hijo):
-        self.hijos.append(nodo_hijo) 
